eb5e359de96b38aba7c200b3f074fdb785fd6ca0
Mandatorytoast <optionaltoast420@gmail.com>
2018-08-07 14:20:33 +0100

    saved placed blocks position

--- tetris.py
+++ tetris.py
@@ -11,6 +11,9 @@ graphic = 'X'
 x_pos = 30
 y_pos = 0
 max_x = 19
+#STARTING COORDINATES FOR ALL THE TETRONIMOES
+#TODO: ADD ROTATED VERSIONS
+#TODO: MAKE FIRST POSITION THE LOWEST POINT ON THE BLOCK
 tetronimo = {"I": [[0,1],[0,2],[0,3],[0,4]],
              "O": [[0,1],[0,2],[1,1],[1,2]],
              "T": [[0,1],[0,2],[0,3],[1,2]],
@@ -19,7 +22,7 @@ tetronimo = {"I": [[0,1],[0,2],[0,3],[0,4]],
              "L": [[0,1],[1,1],[2,1],[2,2]],
              "RL": [[0,2],[1,2],[2,2],[2,1]]}
 
-
+placedblocks = []
 
 blocks = list(tetronimo.keys())
 
@@ -27,7 +30,7 @@ blocks = list(tetronimo.keys())
 c = 0
 
 current_block = copy.deepcopy(tetronimo["T"])
-
+#MAIN GAME LOOP
 while True:
     c = stdscr.getch()
     if c==ord('q'):
@@ -36,15 +39,25 @@ while True:
     for i in current_block:
         stdscr.addstr(i[0],i[1], graphic)
         i[0] += 1
+        #MOVE TETRONIMO LEFT AND RIGHT WHILE FALLING
         if c == curses.KEY_RIGHT:
             i[1] += 1
         elif c == curses.KEY_LEFT:
             i[1] -= 1
-
-    stdscr.addstr(0, 20, "{}:{}:{}".format(current_block[0], max_x, tetronimo["I"]))
+    
+    for i in placedblocks:
+        stdscr.addstr(i[0],i[1], graphic)
+    
+    #DEBUGGING INFO
+    #stdscr.addstr(2, 20, "placed blocks: {}\n".format(placedblocks))
+    #stdscr.addstr(1, 20, "current block: {}".format(current_block))
+    
     
     if current_block[0][0] == max_x:
-       current_block = copy.deepcopy(tetronimo[blocks[random.randint(0,len(blocks)-1)]]) 
+        for i in current_block:
+            placedblocks.append(i)
+
+        current_block = copy.deepcopy(tetronimo[blocks[random.randint(0,len(blocks)-1)]]) 
 
 curses.nocbreak()
 stdscr.keypad(False)
